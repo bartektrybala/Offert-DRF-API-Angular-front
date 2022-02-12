03f042a898db4ecaa3dd80a8135dc3cb7fead5d7
bartek_laptop <bartek@laptop>
2022-02-12 12:13:59 +0100

    feat: inital connection with django

--- frontend/category-offers-app/src/app/app.component.ts
+++ frontend/category-offers-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {GetApiService} from './get-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'category-offers-app';
+  constructor(private api: GetApiService)
+  {
+
+  }
+  ngOnInit()
+  {
+    this.api.apiCall().subscribe((data)=>{
+      console.warn("get api data", data);
+    })
+  }
 }
